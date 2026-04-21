e9c5737c05b492c387cc1549c72a0416c73c9b4e
Alex Tim <alti2712@gmail.com>
2026-04-21 11:11:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    with open(filename) as f:
+        return [
+            line.strip()
+            for line in f
+            if line.strip() and not line.startswith("#")
+        ]
+
 setup(
     name="lidiff",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=parse_requirements("requirements.txt"),
     package_data={
-        "lidiff ": [
+        "lidiff": [ 
             "*.cu",
             "*.cpp",
             "*.h",
